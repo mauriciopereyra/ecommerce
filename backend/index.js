0cead3e72e10bbab994af11d95e297f96e6c0266
root <root@vps565.vpshispeed.net>
2022-11-20 12:49:03 +0100

    Created backend with Express and MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,49 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
+
+const db = mongoose.connection;
+
+var Schema = mongoose.Schema;
+
+var items = mongoose.model('items', new Schema({
+    id: Number,
+    title: String,
+    collectionist: String,
+    price: String,
+    image: String,
+    detailImages: Array,
+    colors: String,
+    sizes: Array,
+    categories: Array,
+}),
+"items");
+
+
+db.on('error', (error) => {
+    console.log(error)
+})
+
+db.once('connected', () => {
+    console.log('Database Connected');
+})
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+app.use('/item/:id', (req,res) => {
+    items.findOne({id: req.params.id},(err,docs) => {
+        if (err) {res.send(err)} else {
+            res.send(docs)
+        }
+    })
+})
+
+
+
+app.listen(3001, () => {
+    console.log(`Server Started at ${3001}`)
 })
